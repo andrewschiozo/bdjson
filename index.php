511c636b1cad5e853a2e3f086829ec741169e9cf
andrewschiozo <andrewschiozo@gmail.com>
2018-07-30 14:02:31 -0300

    Exemplos de uso

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-BdJson
+<?php
+
+require_once 'src/config.php';
+require_once 'src/Table.php';
+require_once 'src/DB.php';
+echo '<pre>';
+try
+{
+	$db = new DB('meubanco');
+	$db->addTable('cliente');
+
+	echo '<br>Data add<br>';
+	$db->getTable('cliente')->addData('cliente 1');
+	$db->getTable('cliente')->addData('cliente 2');
+	$db->getTable('cliente')->addData('cliente 3');
+	print_r($db->getTable('cliente')->getData());
+
+	echo '<br>Data update<br>';
+	$db->getTable('cliente')->updateData(1, 'Segundo cliente');
+	print_r($db->getTable('cliente')->getData());
+
+	$db->getTable('cliente')->clear();
+	$db->getTable('cliente')->delete();
+}
+catch(Exception $e)
+{
+	echo $e->getMessage();
+}
+
+echo "\n\r";
